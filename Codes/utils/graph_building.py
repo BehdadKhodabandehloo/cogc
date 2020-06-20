2acdb878ec340e7795c89f4b12faddb9370cedae
behdad-kh <behdad.khodabandehloo@gmail.com>
2020-06-20 23:44:38 +0430

    retweet graph building

--- Codes/utils/graph_building.py
+++ Codes/utils/graph_building.py
@@ -1,1 +1,25 @@
 # retweet graph building
+
+import networkx as nx
+
+def graph_maker(heads, tails, graph=None):
+    # graph initiate
+    if graph is None:
+        graph = nx.DiGraph()
+    for i in range(len(heads)):
+        if graph.has_edge(tails[i], heads[i]):
+            graph[tails[i]][heads[i]]['weight'] += 1
+        else:
+            graph.add_edges_from([[tails[i], heads[i]]], weight=1)
+
+    return graph
+
+def static_retweet_graph(data, graph=None):
+    heads = []
+    tails = []
+
+    for item in data:
+        heads.append(item['mentionField'])
+        tails.append(item['ownerName'])
+
+    return graph_maker(heads, tails, graph)
