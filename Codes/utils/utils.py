9d914a11ade2405463216c8c23c65a6d938989cc
BehdadKhodabandehloo <behdad.khodabandehloo@gmail.com>
2020-06-22 00:49:50 +0430

    adding nodes texts

---
+++ Codes/utils/utils.py
@@ -0,0 +1,9 @@
+
+def node_texts(data):
+    attributes = {}
+    for i in range(len(data)):
+        key = data.iloc[i]['ownerName']
+        if key in attributes:
+            attributes[key].append(data.iloc[i]['textField'])
+        elif key not in attributes:
+            attributes[key] = [data.iloc[i]['textField']]
