ce7e9af0237e43de91b3e40b47919af7563af652
BehdadKhodabandehloo <behdad.khodabandehloo@gmail.com>
2020-06-22 12:58:52 +0430

    dynamic graph building

--- Codes/utils/graph_building.py
+++ Codes/utils/graph_building.py
@@ -3,26 +3,9 @@
 import networkx as nx
 import pandas as pd
 from utils import *
-
-def graph_maker(heads, tails, graph=None):
-    # graph initiate
-    if graph is None:
-        graph = nx.DiGraph()
-    for i in range(len(heads)):
-        if graph.has_edge(tails[i], heads[i]):
-            graph[tails[i]][heads[i]]['weight'] += 1
-        else:
-            graph.add_edges_from([[tails[i], heads[i]]], weight=1)
-    return graph
-
-
-def static_retweet_graph(data, graph=None):
-    heads = []
-    tails = []
-    for i in range(len(data)):
-        heads.append(data.iloc[i]['mentionField'])
-        tails.append(data.iloc[i]['ownerName'])
-    return graph_maker(heads, tails, graph)
+import dynetx as dn
+from dynetx.readwrite import json_graph
+import json
 
 # how to load from server?
 df = pd.read_csv("") # here we should read raw data
@@ -30,20 +13,42 @@ df.set_index('time_jalali', inplace=True)
 df = df.sort_values(by='time_jalali')
 
 nodes_texts = nodes_texts(data)
-def dynamic_retweet_graph(data, nodes_texts, graph=None):
-    # dynamic graph initialization
-    graph = dn.DynDiGraph()
+
+def graph_maker(data, graph=None):
+    # retweet dynamic directed graph initialization
+    if graph is None:
+        graph = dn.DynDiGraph()
     # add nodes and their attributes to the graph
     for item in nodes_texts.keys():
         graph.add_node(key, text=nodes_texts[item])
-
+    # adding weights to 'graph'
     heads = []
     tails = []
-    timestamps = []
-    texts = []
+    for i in range(len(data)):
+        heads.append(data.iloc[i]['mentionField'])
+        tails.append(data.iloc[i]['ownerName'])
+    for i in range(len(heads)):
+        if graph.has_edge(tails[i], heads[i]):
+            graph[tails[i]][heads[i]]['weight'] += 1
+        else:
+            graph.add_edges_from([[tails[i], heads[i]]], weight=1)
+    # adding interactions to 'graph'
+    for i in range(len(df)):
+        if df.iloc[i]['mentionField'] != '""':
+            graph.add_interaction(u=df.iloc[i]['ownerName'],
+                                  v=df.iloc[i]['mentionField'],
+                                  t=df.iloc[i]['timestamp'])
+    return graph
 
-    
 
+def node_texts(data):
+    attributes = {}
+    for i in range(len(data)):
+        key = data.iloc[i]['ownerName']
+        if key in attributes:
+            attributes[key].append(data.iloc[i]['textField'])
+        elif key not in attributes:
+            attributes[key] = [data.iloc[i]['textField']]
+    return attributes
 
-    return snapshots
 
